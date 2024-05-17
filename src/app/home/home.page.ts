8d4e06a8d1c3c16db4532a1d79f3032d4a1e5bad
hcslearning <learning@hardcybersoft.cl>
2024-05-17 10:15:01 -0400

    cámara funcionando en web

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
+
+  fotos:String[] = []
+
   constructor() {}
+
+  async tomarFoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64
+    })
+
+    if( image.base64String != null || image.base64String != undefined) {
+      this.fotos.push("data:image/jpeg;base64,"+image.base64String)
+      console.dir(this.fotos)
+    }    
+  }
+
 }
