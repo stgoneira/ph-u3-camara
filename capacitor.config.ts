5e2e4f00d0125d9f3af3e2224db39c6b8b2eaba6
hcslearning <learning@hardcybersoft.cl>
2024-05-17 09:47:16 -0400

    proyecto recién iniciado

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ph.u3.camara',
+  appName: 'ph-u3-camara',
+  webDir: 'www'
+};
+
+export default config;
